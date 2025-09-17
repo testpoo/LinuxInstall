22e3de50e0b089372089fa9f63842ca83f947b27
testpoo <402861037@163.com>
2025-09-17 08:54:07 +0800

    添加archsway

---
+++ archsway/README.md
@@ -0,0 +1,99 @@
+title: arch+sway安装设置
+date: 2025-09-17
+category: 系统安装
+tag: debian, sway
+
+[TOC]
+
+### 1. 安装Sway及相关软件
+```
+# 通过arch安装sway
+
+sudo pacman -S wqy-zenhei thunar chromium gvfs fcitx5 fcitx-rime xarchiver git upower blueman thunar-archive-plugin wl-clipboard fcitx5-configtool rime-wubi ristretto libreoffice man unzip xfce-appfinder seatd
+
+sudo pacman -Rns lightdm lightdm-gtk-greeter htop smartmontools vim waybar wget wireless_tools wpa_supplicant openssh pavucontrol wmenu
+
+# 注：安装motrix时，需要安装fuse
+```
+
+### 2. 输入法环境变量设置
+
+启用fcitx输入需要配置环境变量：
+```
+nano /etc/environment
+
+XIM="fcitx"
+#GTK_IM_MODULE=fcitx
+QT_IM_MODULE=fcitx
+XMODIFIERS="@im=fcitx"
+INPUT_METHOD=fcitx
+SDL_IM_MODULE=fcitx
+GLFW_IM_MODULE=fcitx
+```
+
+### 3. 启动Sway
+
+```
+# 编辑 ~/.bash_profile加入以下内容即可：
+
+[ "$(tty)" = "/dev/tty1" ] && exec sway
+```
+
+### 4. 配置sway
+
+```
+mkdir -p ~/.config/sway
+cp /etc/sway/config ~/.config/sway/
+
+# 编辑~/.config/sway
+```
+
+### 5. 修改程序启动器
+
+```
+# 注释掉$menu行，改为以下内容：
+set $menu set $menu xfce4-appfinder
+```
+
+### 6. 设置终端(foot)字体大小
+
+```
+cp -r /etc/foot/ ~/.config/foot/
+vi ~/.config/foot/foot.ini
+
+font=FreeMono:size=12
+```
+
+### 7. 设置分辨率和缩放
+
+```
+swaymsg -t get_outputs
+output eDP-1 resolution 1920x1200 position 0,0 scale 1.25
+```
+
+### 8. 设置触摸板
+
+```
+swaymsg -t get_inputs
+input "2362:597:SYNA3602:00_093A:0255_Touchpad" {
+    dwt enabled
+    tap enabled
+    natural_scroll enabled
+    middle_emulation enabled
+    events enabled
+}
+```
+
+### 9. 其他
+
+```
+# 修改微信文档目录，修改文件/home/poo/.xwechat/config/51a1fffea11325a1e4104c6b3de47af7.ini中使用以下内容
+Appdata:.xwechat
+
+# 若 polkit 未安装在您的系统上，并且您想使用 seatd 来替代，请将您添加到 seat 用户组并启用/启动 seatd.service，然后重新登录。
+sudo usermod -aG seat poo
+
+# 查字体
+ fc-match
+ fc-match -a | grep Mono  # 等线字体
+```
---
+++ archsway/sway/bar.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# 捕获 USR1 信号
+trap 'update_swaybar' SIGUSR1
+
+# 启用JSON协议头
+echo '{"version":1,"click_events":false}'
+echo '[' # 开始JSON数组
+echo '[]' # 初始化空数组
+
+# 主循环生成状态信息
+while true; do
+  # 获取动态数据
+  # 时间
+  now_time=$(date '+%A %F %H:%M 第%V周')
+
+  # 电量
+  battary="⚡️"$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep -i "percentage\|time to empty" | awk -F ':' '{print $2,$4}' | sed 's/ //g') 
+  # battary="ϟ "$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep -i "state\|percentage\|time to empty" | awk -F ':' '{print $2,$4}' | sed 's/ //g') 
+
+  # 亮度
+  light="🔆"$(brightnessctl get | awk '{print $1/192"%"}')
+
+  # 声音
+  volume=$(pactl get-sink-volume @DEFAULT_SINK@ | awk -F '/' '{print $2}' | sed 's/ //g')
+  if [ $(pactl get-sink-mute @DEFAULT_SINK@ | awk -F ' ' '{print $2}') = "no" ];then volume="🔊"$volume;else volume="🔇"$volume;fi
+
+  # CPU
+  cpuUsage="©️"$(top -b -n1 | fgrep "Cpu" | awk '{printf "%d", 100-$8}')"%"
+
+  # 内存
+  mem_used_persent="Ⓜ️"$(free -m | fgrep "内存" | awk '{printf "%.1f",$3/1024}')"G"
+
+  # 磁盘
+  diskUsage="💾"$(df -h | fgrep "/dev/sda2" | awk '{print $3}')
+
+  # 网络
+  if [ $(ip addr | grep inet | awk 'NR==3' | awk -F ' ' '{print $2}'| cut -d '.' -f 1-2) = "192.168" ];then network="📶连接";else network="🌐断开";fi
+
+  # 触摸板
+  touchpad="📋"$(swaymsg -t get_inputs | python3 -c "import os,sys,json; print([li for li in json.load(sys.stdin) if li['identifier'] == '2362:597:SYNA3602:00_093A:0255_Touchpad'][0]['libinput']['send_events'])" | awk '{if ($0 == "enabled"){print "开"} else {print "关"}}')
+
+  # 构造带点击标识的JSON块
+  JSON_BLOCKS=$(cat <<EOF
+,[
+  {"full_text":"$cpuUsage","border":"#111111aa","border_left": 3,"border_right":3,"align":"center","background":"#111111aa","separator":false,"separator_block_width":3,"color":"#ffffff"},
+  {"full_text":"$mem_used_persent","border":"#111111aa","border_left": 3,"border_right":3,"align":"center","background":"#111111aa","separator":false,"separator_block_width":3,"color":"#ffffff"},
+  {"full_text":"$diskUsage","border":"#111111aa","border_left": 3,"border_right":3,"align":"center","background":"#111111aa","separator":false,"separator_block_width":3,"color":"#ffffff"},
+  {"full_text":"$touchpad","border":"#111111aa","border_left": 3,"border_right":3,"align":"center","background":"#111111aa","separator":false,"separator_block_width":3,"color":"#ffffff"},
+  {"full_text":"$light","border":"#111111aa","border_left": 3,"border_right":3,"align":"center","background":"#111111aa","separator":false,"separator_block_width":3,"color":"#ffffff"},
+  {"full_text":"$volume","border":"#111111aa","border_left": 3,"border_right":3,"align":"center","background":"#111111aa","separator":false,"separator_block_width":3,"color":"#ffffff"},
+  {"full_text":"$network","border":"#111111aa","border_left": 3,"border_right":3,"align":"center","background":"#111111aa","separator":false,"separator_block_width":3,"color":"#ffffff"},
+  {"full_text":"$battary","border":"#111111aa","border_left": 3,"border_right":3,"align":"center","background":"#111111aa","separator":false,"separator_block_width":3,"color":"#ffffff"},
+  {"full_text":"$now_time","border":"#111111aa","border_left": 3,"border_right":3,"align":"center","background":"#111111aa","separator":false,"separator_block_width":3,"color":"#ffffff"}
+]
+EOF
+  )
+
+  echo "$JSON_BLOCKS"  
+  sleep 5 &
+  wait $!
+done
