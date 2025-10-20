408ad0b071c1c8f63c88dd94d43f861162ec9346
testpoo <402861037@163.com>
2025-10-20 21:24:04 +0800

    更新配置

--- debianlabwc/.config/labwc/ltouchpad.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if grep -q '<sendEventsMode>disabledOnExternalMouse</sendEventsMode>$' ~/.config/labwc/rc.xml; then
-    sed -i 's/<sendEventsMode>disabledOnExternalMouse<\/sendEventsMode>/<sendEventsMode>yes<\/sendEventsMode>/' ~/.config/labwc/rc.xml
-    labwc -r
-else
-    sed -i 's/<sendEventsMode>yes<\/sendEventsMode>/<sendEventsMode>disabledOnExternalMouse<\/sendEventsMode>/' ~/.config/labwc/rc.xml
-    labwc -r
-fi
