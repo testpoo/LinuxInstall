123272a06c70ac6d9fcb9a79558988316e5d62ba
testpoo <402861037@163.com>
2026-02-04 19:03:40 +0800

    更新配置

--- archlabwc/.config/labwc/menu.py
+++ archlabwc/.config/labwc/menu.py
@@ -3,12 +3,139 @@
 
 import os,configparser
 
+del_cats = {
+    'X-XFCE',
+    'HardwareSettings',
+    'X-XFCE-SettingsDialog',
+    'GTK',
+    'Qt',
+    'TerminalEmulator',
+    'DesktopSettings',
+    'X-XFCE-PersonalSettings',
+    'WordProcessor',
+    'X-Red-Hat-Base',
+    'WebBrowser',
+    'X-Xfce-Toplevel',
+    'GNOME',
+    'X-SuSE-Core-Office',
+    '2DGraphics',
+    'Database',
+    'FlowChart',
+    'VectorGraphics',
+    'FileTools',
+    'FileManager',
+    'Spreadsheet',
+    'Archiving',
+    'Compression',
+    'Presentation',
+    'Core',
+    'XFCE',
+    'Math',
+    'Viewer',
+    'Printing',
+    'TextEditor',
+    'Filesystem'
+}
+
+cats_zh = {
+  "Office": [
+    "办公",
+    "applications-office"
+  ],
+  "Development": [
+    "开发",
+    "applications-development"
+  ],
+  "Graphics": [
+    "图形",
+    "applications-graphics"
+  ],
+  "Network": [
+    "网络",
+    "applications-network"
+  ],
+  "Education": [
+    "教育",
+    "applications-education"
+  ],
+  "Science": [
+    "科学",
+    "applications-science"
+  ],
+  "Games": [
+    "游戏",
+    "applications-games"
+  ],
+  "System": [
+    "系统",
+    "applications-system"
+  ],
+  "Utility": [
+    "实用工具",
+    "applications-utilities"
+  ],
+  "Settings": [
+    "设置",
+    "settings"
+  ],
+  "AudioVideo": [
+    "多媒体",
+    "audio"
+  ]
+}
+del_files = {
+    'org.gnupg.pinentry-qt5.desktop',
+    'bssh.desktop',
+    'org.gnupg.pinentry-qt.desktop',
+    'bvnc.desktop',
+    'avahi-discover.desktop'
+}
+
+logouts = [
+  {
+    "Name": "锁屏",
+    "Exec": "swaylock -f -i /home/poo/图片/background.png -c 015000",
+    "Icon": "system-lock-screen"
+  },
+  {
+    "Name": "挂起",
+    "Exec": "systemctl suspend",
+    "Icon": "system-suspend"
+  },
+  {
+    "Name": "休眼",
+    "Exec": "systemctl hibernate",
+    "Icon": "system-suspend-hibernate"
+  },
+  {
+    "Name": "重载",
+    "Exec": "labwc -r",
+    "Icon": "systemback"
+  },
+  {
+    "Name": "退出",
+    "Exec": "labwc -e",
+    "Icon": "system-log-out"
+  },
+  {
+    "Name": "重启",
+    "Exec": "systemctl reboot",
+    "Icon": "system-reboot"
+  },
+  {
+    "Name": "关机",
+    "Exec": "systemctl poweroff -i",
+    "Icon": "system-shutdown"
+  }
+]
+
 file_path = '/usr/share/applications/'
 file_names = os.listdir(file_path)
-file_names = set(file_names) - {'org.gnupg.pinentry-qt5.desktop','bssh.desktop','org.gnupg.pinentry-qt.desktop','bvnc.desktop','avahi-discover.desktop'}
+file_names = set(file_names) - del_files
 
 app_lists = []
 cat_lists = []
+app_dicts = {}
 
 for name in file_names:
     config = configparser.RawConfigParser()
@@ -23,7 +150,7 @@ for name in file_names:
     else:
         continue
     if config.has_option('Desktop Entry','Exec'):
-        Exec = config.get('Desktop Entry','Exec').split(" ")[0]
+        Exec = config.get('Desktop Entry','Exec').replace('%F','').replace('%f','').replace('%U','')
     else:
         continue
     if config.has_option('Desktop Entry','Icon'):
@@ -37,18 +164,7 @@ for name in file_names:
     app_lists.append({"Name":Name,"Exec":Exec,"Icon":Icon,"Categories":Categories})
     cat_lists.extend(Categories)
 
-del_cats = {'X-XFCE','HardwareSettings','X-XFCE-SettingsDialog','GTK','Qt','TerminalEmulator','DesktopSettings','X-XFCE-PersonalSettings','WordProcessor','X-Red-Hat-Base','WebBrowser','X-Xfce-Toplevel','GNOME','X-SuSE-Core-Office','2DGraphics','Database','FlowChart','VectorGraphics','FileTools','FileManager','Spreadsheet','Archiving','Compression','Presentation','Core','XFCE','Math','Viewer','Printing','TextEditor','Filesystem'}
-app_lists.append({"Name":" 锁屏    ","Exec":"swaylock -f -i /home/poo/图片/background.png -c 015000","Icon":"system-lock-screen","Categories":['Logout']})
-app_lists.append({"Name":" 挂起    ","Exec":"systemctl suspend","Icon":"system-suspend","Categories":['Logout']})
-app_lists.append({"Name":" 休眼    ","Exec":"systemctl hibernate","Icon":"system-suspend-hibernate","Categories":['Logout']})
-app_lists.append({"Name":" 重载    ","Exec":"labwc -r","Icon":"systemback","Categories":['Logout']})
-app_lists.append({"Name":" 退出    ","Exec":"labwc -e","Icon":"system-log-out","Categories":['Logout']})
-app_lists.append({"Name":" 重启    ","Exec":"systemctl reboot","Icon":"system-reboot","Categories":['Logout']})
-app_lists.append({"Name":" 关机    ","Exec":"systemctl poweroff -i","Icon":"system-shutdown","Categories":['Logout']})
-
-app_dicts = {}
-cats_zh = {"Office":["办公","applications-office"],  "Development":["开发","applications-development"],  "Graphics":["图形","applications-graphics"], "Network":["网络","applications-network"],  "Education":["教育","applications-education"], "Science":["科学","applications-science"], "Games":["游戏","applications-games"], "System":["系统","applications-system"],  "Utility":["实用工具","applications-utilities"],  "Settings":["设置","settings"],  "Logout":["退出","gnome-logout"], "AudioVideo":["多媒体","audio"]}
-cats = list(set(cat_lists) - del_cats) + ['Logout']
+cats = list(set(cat_lists) - del_cats)
 
 for app in app_lists:
     for cat in cats:
@@ -57,6 +173,8 @@ for app in app_lists:
                 app_dicts[cat] = app_dicts[cat] + [app]
             else:
                 app_dicts[cat] = [app]
+
+app_dicts = dict(sorted(app_dicts.items(), key=lambda item: item[0]))
 print('<openbox_pipe_menu>')
 for key in app_dicts:
     if key in cats_zh:
@@ -68,4 +186,9 @@ for key in app_dicts:
         print('<action name="Execute" command="' + app["Exec"] + '" />')
         print('</item>')
     print('</menu>')
+print("<separator />")
+for app in logouts:
+    print('<item label="' + app["Name"] + '" icon="' + app["Icon"] + '">')
+    print('<action name="Execute" command="' + app["Exec"] + '" />')
+    print('</item>')
 print('</openbox_pipe_menu>')
